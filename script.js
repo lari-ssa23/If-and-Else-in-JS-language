166a23263ca1fb95da35296e8a9b02d7b3c8db8a
lari-ssa23 <larissa.isabel@unesp.br>
2022-01-17 22:42:15 -0300

    desafio de If e Else

---
+++ script.js
@@ -0,0 +1,33 @@
+let idade=22
+if(idade>=18){
+    console.log("Pode levar o goró. Você é maior de idade.")
+}
+
+let humano=true
+if(idade>= 18 && humano==true){
+    console.log("Você é um ser humaninho que responde por seus atos")
+}
+
+let mesDeNiver="dezembro"
+if(mesDeNiver=="janeiro" || mesDeNiver=="dezembro"){
+    console.log("Parabens, você faz aniversario enquanto todos estão viajando")
+}
+
+let Inicial="L"
+if(Inicial=="R"){
+    console.log("Seu nome começa com a décima oitava letra do alfabeto")
+} else{
+    console.log("Seu nome não começa com R")
+}
+
+//1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+
+//2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+
+//3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+
+
+//*** Exercícios bônus
+//4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+//5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
